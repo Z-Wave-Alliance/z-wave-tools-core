24c5ff987e80db2002e58eb6e2a0ea8365577f96
Sebastian Loeffler <sebastian.loeffler@mk-logic.de>
2025-11-10 09:37:45 +0100

    Clarify NodeTypes enum. Make sure it is a byte.
    Fixes the issue where the enum could not be simply parsed back into a byte for
    the Z-Wave Plus Info Report.

--- ZWave/Enums/NodeTypes.cs
+++ ZWave/Enums/NodeTypes.cs
@@ -1,8 +1,9 @@
 /// SPDX-License-Identifier: BSD-3-Clause
 /// SPDX-FileCopyrightText: Silicon Laboratories Inc. https://www.silabs.com
+/// SPDX-FileCopyrightText: Z-Wave Alliance https://z-wavealliance.org
 namespace ZWave.Enums
 {
-    public enum NodeTypes
+    public enum NodeTypes : byte
     {
         None = 0xFF,
         ZWAVEPLUS_NODE = 0x00,
